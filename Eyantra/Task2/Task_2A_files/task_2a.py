03211d3d64ca92652a786913f3a2829edfebc227
Soham Rajesh Pawar <121933361+srp1506@users.noreply.github.com>
2024-07-06 15:28:55 +0530

    Add files via upload

---
+++ Eyantra/Task2/Task_2A_files/task_2a.py
@@ -0,0 +1,201 @@
+'''
+*****************************************************************************************
+*
+*        		===============================================
+*           		Geo Guide (GG) Theme (eYRC 2023-24)
+*        		===============================================
+*
+*  This script is to implement Task 2A of Geo Guide (GG) Theme (eYRC 2023-24).
+*  
+*  This software is made available on an "AS IS WHERE IS BASIS".
+*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
+*  any and all claim(s) that emanate from the use of the Software or 
+*  breach of the terms of this agreement.
+*
+*****************************************************************************************
+'''
+
+# Team ID:			[ Team-ID ]
+# Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
+# Filename:			task_2a.py
+# Functions:		detect_ArUco_details
+# 					[ Comma separated list of functions in this file ]
+
+
+####################### IMPORT MODULES #######################
+## You are not allowed to make any changes in this section. ##
+## You have to implement this task with the five available  ##
+## modules for this task                                    ##
+##############################################################
+import numpy as np
+import cv2
+from cv2 import aruco
+import math
+##############################################################
+
+################# ADD UTILITY FUNCTIONS HERE #################
+
+
+def calculate_marker_center(corners):
+    corners = np.array(corners)
+    avg_x = np.mean(corners[:, 0])
+    avg_y = np.mean(corners[:, 1])
+    return int(avg_x), int(avg_y)
+
+def calculate_orientation(corners):
+    corners = np.array(corners)
+    
+    top_left = corners[0]
+    top_right = corners[1]
+
+    vector_top = top_right - top_left
+
+    angle_rad = np.arctan2(vector_top[1], vector_top[0])
+    angle_deg = round(np.degrees(angle_rad))
+
+    return angle_deg
+
+
+##############################################################
+
+def detect_ArUco_details(image):
+
+    """
+    Purpose:
+    ---
+    This function takes the image as an argument and returns two dictionaries where one
+    contains details regarding the center coordinates and orientation of the marker
+    and the second dictionary contains values of the 4 corner coordinates of the marker. 
+    
+    First output: The dictionary `ArUco_details_dict` should should have the id of the marker 
+    as the key and the value corresponding to that id should be a list containing the following details
+    in this order: [[center_x, center_y], angle from the vertical]     
+    This order should be strictly maintained in the output
+    Datatypes:
+    1. id - int
+    2. center coordinates - int
+    3. angle - int, x and y coordinates should be combined as a list for each corner
+
+    Second output: The dictionary `ArUco_corners` should contain the id of the marker as key and the
+    corresponding value should be an array of the coordinates of 4 corner points of the markers
+    Datatypes:
+    1. id - int
+    2. corner coordinates - each coordinate value should be float, x and y coordinates should 
+    be combined as a list for each corner
+
+    Input Arguments:
+    ---
+    `image` :	[ numpy array ]
+            numpy array of image returned by cv2 library
+    Returns:
+    ---
+    `ArUco_details_dict` : { dictionary }
+            dictionary containing the details regarding the ArUco marker
+
+    `ArUco_corners` : { dictionary }
+            dictionary containing the details regarding the corner coordinates of the ArUco marker
+    
+    Example call:
+    ---
+    ArUco_details_dict, ArUco_corners = detect_ArUco_details(image)
+
+    Example output for 2 markers in an image:
+    ---
+    * ArUco_details_dict = {9: [[311, 490], 0], 3: [[158, 175], -22]}
+    * ArUco_corners = 
+       {9: array([[211., 389.],
+       [412., 389.],
+       [412., 592.],
+       [211., 592.]], dtype=float32), 
+       3: array([[109.,  46.],
+       [284., 118.],
+       [207., 304.],
+       [ 33., 232.]], dtype=float32)}
+    """    
+    ArUco_details_dict = {}
+    ArUco_corners = {}
+    
+    ##############	ADD YOUR CODE HERE	##############
+    global marker_id
+    marker_id = 0
+    
+    # print("OpenCV version:", cv2.version)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
+    # aruco_dict = cv2.aruco.Dictionary_create(6, 250, markerSize = 1)
+    # print("hi")
+    parameters = cv2.aruco.DetectorParameters()
+    # parameters = cv2.aruco.DetectorParameters_create()
+    # print("hi")
+    _, thresholded = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    corners_tuple, ids, _ = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    # print("hi")
+    corners = [element.reshape(-1, 2).tolist() for element in corners_tuple]
+    
+    # for i in range(len(corners)):
+    #     corners[i].tolist()
+    #print(type(corners), corners)
+        
+    if ids is not None:
+        #print(type(corners[0]),corners[0], corners[1], corners[2])
+        for i in range(len(ids)):
+                
+            #print(type(ids), ids, type(corners), corners)
+            center_x, center_y = calculate_marker_center(corners[i])
+            orientation = calculate_orientation(corners[i])
+            ArUco_details_dict[marker_id] = [[center_x, center_y], orientation]
+            #print(corners, type(corners))
+            ArUco_corners[marker_id] = np.array(corners[i], dtype = np.float32)
+            marker_id = marker_id + 1
+    ##################################################
+    #print(ArUco_corners, ArUco_details_dict)
+    return ArUco_details_dict, ArUco_corners 
+
+######### YOU ARE NOT ALLOWED TO MAKE CHANGES TO THE CODE BELOW #########		
+
+def mark_ArUco_image(image,ArUco_details_dict, ArUco_corners):
+
+    for ids, details in ArUco_details_dict.items():
+        center = details[0]
+        cv2.circle(image, center, 5, (0,0,255), -1)
+
+        corner = ArUco_corners[int(ids)]
+        cv2.circle(image, (int(corner[0][0]), int(corner[0][1])), 5, (50, 50, 50), -1)
+        cv2.circle(image, (int(corner[1][0]), int(corner[1][1])), 5, (0, 255, 0), -1)
+        cv2.circle(image, (int(corner[2][0]), int(corner[2][1])), 5, (128, 0, 255), -1)
+        cv2.circle(image, (int(corner[3][0]), int(corner[3][1])), 5, (25, 255, 255), -1)
+
+        tl_tr_center_x = int((corner[0][0] + corner[1][0]) / 2)
+        tl_tr_center_y = int((corner[0][1] + corner[1][1]) / 2) 
+
+        cv2.line(image,center,(tl_tr_center_x, tl_tr_center_y),(255,0,0),5)
+        display_offset = int(math.sqrt((tl_tr_center_x - center[0])**2+(tl_tr_center_y - center[1])**2))
+        cv2.putText(image,str(ids),(center[0]+int(display_offset/2),center[1]),cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+        angle = details[1]
+        cv2.putText(image,str(angle),(center[0]-display_offset,center[1]),cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+    return image
+
+if __name__ == "__main__":
+
+    # path directory of images in test_images folder
+    img_dir_path = "public_test_cases/"
+
+    marker = 'aruco'
+
+    for file_num in range(0,2):
+        img_file_path = img_dir_path +  marker + '_' + str(file_num) + '.png'
+
+        # read image using opencv
+        img = cv2.imread(img_file_path)
+
+        print('\n============================================')
+        print('\nFor '+ marker  +  str(file_num) + '.png')
+   
+        ArUco_details_dict, ArUco_corners = detect_ArUco_details(img)
+        print("Detected details of ArUco: " , ArUco_details_dict)
+
+        #displaying the marked image
+        img = mark_ArUco_image(img, ArUco_details_dict, ArUco_corners) 
+        cv2.imshow("Marked Image",img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
